1eb7571ff597a9bc6332977df1b6f7e518672692
aldarial <dariapersikk@gmail.com>
2023-08-20 14:03:09 +0200

    hm31

---
+++ homework1.js
@@ -0,0 +1,12 @@
+var first_number = parseFloat(prompt("Enter the first number: "));
+var second_number = parseFloat(prompt("Enter the second number: "));
+
+var add_result = first_number + second_number;
+var sub_result = first_number - second_number;
+var multi_result = first_number * second_number;
+var div_result = first_number / second_number;
+
+alert(first_number + " + " + second_number + " = " + add_result);
+alert(first_number + " - " + second_number + " = " + sub_result);
+alert(first_number + " * " + second_number  + " = " + multi_result);
+alert(first_number + " / " + second_number  + " = " + div_result);
